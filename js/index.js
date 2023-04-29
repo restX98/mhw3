c5c265de914cf6525fd388551d6d0f07f0b36316
Enrico Restuccia <enrico.rest98@gmail.com>
2023-04-29 13:38:42 +0200

    Rework handlers

--- js/index.js
+++ js/index.js
@@ -1,33 +1,5 @@
-function selectSongHandler(e) {
-  const embedIFrame = document.querySelector("#embed-iframe");
-  const songsBox = document.querySelector(".player");
-  const target = e.currentTarget;
-  const options = {
-    width: "100%",
-    height: "80px",
-    uri: target.dataset.uri,
-  };
-  const callback = (EmbedController) => {
-    document.querySelectorAll(".song").forEach((song) => {
-      song.removeEventListener("click", selectSongHandler);
-      song.addEventListener("click", () => {
-        EmbedController.loadUri(song.dataset.uri);
-        document.querySelector(".song.active").classList.remove("active");
-        song.classList.add("active");
-      });
-    });
-  };
-  SpotifyIframeApi.createController(embedIFrame, options, callback);
-  target.classList.add("active");
-  songsBox.style.display = "block";
-}
-
-function searchHandler(e) {
-  e.preventDefault();
-  const form = e.target;
-  const { track, artist } = serialize(form);
-  searchForItems(track, artist, updateSongs);
-}
-
 const searchForm = document.querySelector(".search form");
 searchForm.addEventListener("submit", searchHandler);
+
+const generateImagesBtn = document.querySelector("#generateImage");
+generateImagesBtn.addEventListener("click", generateImagesHandler);
