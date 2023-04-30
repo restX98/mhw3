dbac437dbc8dec74bde218ea753a74db953edf5c
Enrico Restuccia <enrico.rest98@gmail.com>
2023-04-30 12:17:58 +0200

    Adding Constants

--- js/constants.js
+++ js/constants.js
@@ -1,8 +1,8 @@
 const SpotifyCredentials = {
-  clientId: "",
-  clientSecret: "",
+  clientId: "702f5406e3b941f881740d49bf51bbd9",
+  clientSecret: "462d18496fe94741889a988a5053cb42",
 };
 
 const OpenAICredentials = {
-  apiKey: "",
+  apiKey: "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",
 };
